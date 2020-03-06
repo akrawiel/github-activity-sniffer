21f24eae203429a474bb7856c4e4f4926fb50a49
Aleksander Krawiel <a.krawiel@gmail.com>
2020-03-06 10:44:59 +0100

    Add activity item animations

--- src/components/ActivityItem.vue
+++ src/components/ActivityItem.vue
@@ -1,19 +1,20 @@
 <template lang="pug">
-.activity-item
-  img.avatar(:src="member.avatar_url")
-  .member-login.flex.font-bold.items-center.justify-start.text-xl 
-    a.link(:href="memberProfileHref" target="_blank") {{ member.login }}
-  .latest-event-container
-    .latest-event-date.gap-2.grid.grid-columns-manual.justify-between.w-full
-      b Latest event date:&emsp;
-      span.truncate.text-right {{ latestEventDate }}
-    .latest-event-type.gap-2.grid.grid-columns-manual.justify-between.w-full
-      b Latest event type:&emsp;
-      span.truncate.text-right {{ latestEventType }}
-    .latest-event-repo.gap-2.grid.grid-columns-manual.justify-between.w-full
-      b Latest event repo:&emsp;
-      a.link.truncate.text-right(:href="latestEventRepoHref" target="_blank" v-if="latestEventRepoHref") {{ latestEventRepoName }}
-      span.truncate.text-right(v-else) {{ latestEventRepoName }}
+transition(name="collapse" appear)
+  .activity-item
+    img.avatar(:src="member.avatar_url")
+    .member-login.flex.font-bold.items-center.justify-start.text-xl 
+      a.link(:href="memberProfileHref" target="_blank") {{ member.login }}
+    .latest-event-container
+      .latest-event-date.gap-2.grid.grid-columns-manual.justify-between.w-full
+        b Latest event date:&emsp;
+        span.truncate.text-right {{ latestEventDate }}
+      .latest-event-type.gap-2.grid.grid-columns-manual.justify-between.w-full
+        b Latest event type:&emsp;
+        span.truncate.text-right {{ latestEventType }}
+      .latest-event-repo.gap-2.grid.grid-columns-manual.justify-between.w-full
+        b Latest event repo:&emsp;
+        a.link.truncate.text-right(:href="latestEventRepoHref" target="_blank" v-if="latestEventRepoHref") {{ latestEventRepoName }}
+        span.truncate.text-right(v-else) {{ latestEventRepoName }}
 </template>
 
 <script>
@@ -50,11 +51,12 @@ export default {
 
 <style lang="postcss" scoped>
 $avatar_size: 5rem;
+$item_height: 6.25rem;
 
 .activity-item {
-  @apply grid gap-4 items-center p-2 w-full;
+  @apply grid gap-4 items-center overflow-hidden p-2 w-full;
   grid-template-columns: $avatar_size 0.5fr 0.5fr;
-  min-height: $avatar_size;
+  height: $item_height;
 
   @media screen and (max-width: theme("screens.sm")) {
     grid-template-columns: 1fr;
@@ -65,6 +67,20 @@ $avatar_size: 5rem;
   @apply border-4 border-solid border-blue-400 rounded-full shadow-lg;
   height: $avatar_size;
   width: $avatar_size;
+}
+
+.collapse-enter,
+.collapse-leave-to {
+  height: 0;
+}
+
+.collapse-enter-to,
+.collapse-leave {
+  height: $item_height;
+}
+
+.collapse-enter-active {
+  transition: height 0.25s ease-out;
 }
 
 .latest-event-container {
